d7e899575be563950f9013841d9ff429b8b737ea
SmoLL-iCe <smoll.w.ice@gmail.com>
2018-02-01 12:44:52 -0200

    update to x64
    update to x64

--- ScanMemory.cs
+++ ScanMemory.cs
@@ -8,22 +8,44 @@ using System.Diagnostics;
 
 using System.IO;
 using Microsoft.Win32.SafeHandles;
+using System.Windows.Forms;
+
 public class DotNetScanMemory_SmoLL
 {
 
     [DllImport("kernel32.dll")]
     public static extern uint GetLastError();
+    //[DllImport("kernel32.dll")]
+    //public static extern int OpenProcess(uint dwDesiredAccess, bool bInheritHandle, int dwProcessId);
     [DllImport("kernel32.dll")]
-    public static extern int OpenProcess(uint dwDesiredAccess, bool bInheritHandle, int dwProcessId);
-    [DllImport("kernel32.dll")]
-    protected static extern bool ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] buffer, uint size, int lpNumberOfBytesRead);
+    protected static extern bool ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] buffer, uint size, uint lpNumberOfBytesRead);
 
     [DllImport("kernel32.dll")]
-    public static extern bool WriteProcessMemory(int hProcess, int lpBaseAddress, byte[] buffer, int size, int lpNumberOfBytesWritten);
+    public static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] buffer, uint size, uint lpNumberOfBytesWritten);
 
     [DllImport("kernel32.dll", SetLastError = true)]
-    protected static extern int VirtualQueryEx(IntPtr hProcess, IntPtr lpAddress, out MEMORY_BASIC_INFORMATION lpBuffer, int dwLength);
+    protected static extern int VirtualQueryEx(IntPtr hProcess, IntPtr lpAddress, out MEMORY_BASIC_INFORMATION lpBuffer, uint dwLength);
 
+
+
+#if WIN64 
+    //estrutura para x64
+    [StructLayout(LayoutKind.Sequential)]
+    public struct MEMORY_BASIC_INFORMATION
+    {
+        public ulong BaseAddress;
+        public ulong AllocationBase;
+        public int AllocationProtect;
+        public int __alignment1;
+        public ulong RegionSize;
+        public int State;
+        public int Protect;
+        public int Type;
+        public int __alignment2;
+    }
+    //http://www.pinvoke.net/default.aspx/kernel32.virtualqueryex
+#else
+    //estrutura para x86
     [StructLayout(LayoutKind.Sequential)]
     protected struct MEMORY_BASIC_INFORMATION
     {
@@ -35,6 +57,9 @@ public class DotNetScanMemory_SmoLL
         public uint Protect;
         public uint Type;
     }
+#endif
+
+
     List<MEMORY_BASIC_INFORMATION> MappedMemory { get; set; }
 
 
@@ -66,12 +91,15 @@ public class DotNetScanMemory_SmoLL
         MEM_RESERVE = 0x2000
     }
 
+
     private enum TypeEnum : uint
     {
         MEM_IMAGE = 0x1000000,
         MEM_MAPPED = 0x40000,
         MEM_PRIVATE = 0x20000
     }
+
+
     public static string GetSystemMessage(uint errorCode)
     {
         var exception = new System.ComponentModel.Win32Exception((int)errorCode);
@@ -79,7 +107,11 @@ public class DotNetScanMemory_SmoLL
     }
     //######################## VARS #############################
     public UInt64 InicioScan = 0x0;
-    public UInt64 FimScan = 0xFFFFFFFF;
+#if WIN64
+    public UInt64 FimScan = 0x7fffffffffffffff;//limite de x64
+#else
+    public UInt64 FimScan = 0xFFFFFFFF;//limite de x64
+#endif
     Boolean StopTheFirst = false;
     Process Attacked;
     List<IntPtr> AddressList = new List<IntPtr>();
@@ -87,23 +119,27 @@ public class DotNetScanMemory_SmoLL
 
     protected void MemInfo(IntPtr pHandle)
     {
-        IntPtr Addy = new IntPtr();
-        Addy = (IntPtr)InicioScan;
+        IntPtr mAddress = new IntPtr();
+        mAddress = (IntPtr)InicioScan; 
         while (true)
         {
-            if ((UInt64)Addy > FimScan)
-            {
-                break;
-            }
+            //Logs.WriteLog("teste");
+            if ((UInt64)mAddress > FimScan) break;           
             var MBI = new MEMORY_BASIC_INFORMATION();
-            int MemDump = VirtualQueryEx(pHandle, Addy, out MBI, Marshal.SizeOf(MBI));
+            int MemDump = VirtualQueryEx(pHandle, mAddress, out MBI, (uint)Marshal.SizeOf(MBI));
+            //Logs.WriteLog(MemDump.ToString());
             if (MemDump == 0) break;
-            if (((uint)MBI.State & (uint)StateEnum.MEM_COMMIT) != 0 && 
-                !(((uint)MBI.Protect & (uint)AllocationProtectEnum.PAGE_GUARD)
-                == (uint)AllocationProtectEnum.PAGE_GUARD))
-                MappedMemory.Add(MBI);
+            if ((MBI.State & (uint)StateEnum.MEM_COMMIT) != 0 && //paginas que contem a memoria do armazenamento físico 
+                !((MBI.Protect & (uint)AllocationProtectEnum.PAGE_GUARD) 
+                == (uint)AllocationProtectEnum.PAGE_GUARD))//evita paginas de guarda
+                MappedMemory.Add(MBI);//lista de mapeamento da memoria 
+#if WIN64
+            mAddress = (IntPtr)(MBI.BaseAddress + MBI.RegionSize);
+#else
+            mAddress = new IntPtr(MBI.BaseAddress.ToInt64() + MBI.RegionSize);
+#endif
 
-            Addy = new IntPtr(MBI.BaseAddress.ToInt32() + (int)MBI.RegionSize);
+      
         }
     }
 
@@ -119,29 +155,35 @@ public class DotNetScanMemory_SmoLL
 
     protected IntPtr ScanInBuff(IntPtr Address,byte[] Buff, string[] StrMask)
     {
-        int TamanhoBuf = Buff.Length;
+        Int64 TamanhoBuf = Buff.Length;
         int TamanhoScan = StrMask.Length;
         int TScan = TamanhoScan - 1;
         byte[] SigScan = new byte[TamanhoScan];
         for (int i = 0; i < TamanhoScan; i++)
         {
-            if (StrMask[i] == "??")
+            if (StrMask[i] == "??") // bytes indefinitos altera pra zero 
                 SigScan[i] = 0x0;
             else
-                SigScan[i] = Convert.ToByte(StrMask[i], 16);
+                SigScan[i] = Convert.ToByte(StrMask[i], 16); //converte byte a byte da mascara 
         }
-        int go = 0;
+
+        Int64 go = 0;
         while (go <= (TamanhoBuf - TamanhoScan - 1))
         {
-            if (Buff[go] == SigScan[0])
+            if (Buff[go] == SigScan[0])//confere se o primeiro byte do array é igual o do ponto atual do buffer  
             {
-                for (int i = TScan; ((StrMask[i] == "??") || (Buff[go + i] == SigScan[i])); i--)
-                    if (i == 0)
+                for (int i = TScan; ((StrMask[i] == "??")/* ou a mascara nesses byte é indefinido ou*/ || 
+                    (Buff[go + i] == SigScan[i]))/* ou os bytes são iguais*/; i--/*vai reduzindo o valor do tamanho do array*/)
+                    if (i == 0)//chegou a zero, achou o array
                     {
-                        if (StopTheFirst)
+                        if (StopTheFirst)//parar ao encontrar o primeiro ? 
                             return new IntPtr(go);
-                        else
-                            AddressList.Add((IntPtr)(Address.ToInt32() + go));
+                        else {
+                            if ((UInt64)(Address.ToInt64() + go) >= InicioScan && 
+                                (UInt64)(Address.ToInt64() + go) <= FimScan)
+                                AddressList.Add((IntPtr)(Address.ToInt64() + go));//adiciona a lista com os endereços encontrados
+                        }
+                            
                         break;
                     }
 
@@ -162,43 +204,61 @@ public class DotNetScanMemory_SmoLL
         {
 
         }
-        return (Process)null;
+        return null;
     }
-    public IntPtr[] ScanArray(Process P,string ArrayString )
-    {
-        EnablePrivileges.GoDebugPriv();
-        IntPtr[] Retorna = new IntPtr[1];
-        Logs.DeleteLog();
-        if (P == (Process)null)
-        {
-            Retorna = new IntPtr[1];
-            return Retorna;
-        }else
-            Attacked = Process.GetProcessById(P.Id); //ReCheck Pos Privileges
-        String[] BytesToScan = ArrayString.Split(" "[0]);
-        for (int i = 0; i < BytesToScan.Length; i++)
-            if (BytesToScan[i] == "?")
-                BytesToScan[i] = "??";
 
+
+
+    IntPtr[] Retorna =  null;
+    string ArrayString;
+    public IntPtr[] ScanArray(Process P, string ArrayString_) {
+        EnablePrivileges.GoDebugPriv();//Define o privilégio de depuração       
+        //Logs.DeleteLog();
+       
+        if (P == null)//se não encontrar o processo       
+            return Retorna;        
+        else
+            Attacked = Process.GetProcessById(P.Id); //ReCheck Pos Privileges
+
+        ArrayString = ArrayString_;
+        StartScan();
+        return Retorna;  
+    }
+
+
+
+    void StartScan()
+    {
 
         
-        MappedMemory = new List<MEMORY_BASIC_INFORMATION>();
-        MemInfo(Attacked.Handle);
-        for (int i = 0; i < MappedMemory.Count; i++)
-        {
+        String[] BytesToScan = ArrayString.Split(" "[0]);
+        for (int i = 0; i < BytesToScan.Length; i++)
+            if (BytesToScan[i] == "?")//caso informou apenas um imterrogação 
+                BytesToScan[i] = "??";
+      
+        MappedMemory = new List<MEMORY_BASIC_INFORMATION>();//cria uma lista pra salvar o mapa da memoria
+        MemInfo(Attacked.Handle);//faz o mapeamento
+        for (int i = 0; i < MappedMemory.Count; i++)//procurar em cada região
+        {          
+            byte[] buff = new byte[MappedMemory[i].RegionSize];//define o tamanho do buffer com o tamanho da região
            
-            byte[] buff = new byte[MappedMemory[i].RegionSize];
-            ReadProcessMemory(Attacked.Handle, MappedMemory[i].BaseAddress, buff, MappedMemory[i].RegionSize, 0);
+            ReadProcessMemory(Attacked.Handle, (IntPtr)MappedMemory[i].BaseAddress, buff, (uint)MappedMemory[i].RegionSize, 0);
             IntPtr Result = IntPtr.Zero;
             if (buff.Length > 0)
-                Result = ScanInBuff(MappedMemory[i].BaseAddress,buff, BytesToScan);
+                Result = ScanInBuff((IntPtr)MappedMemory[i].BaseAddress,buff, BytesToScan);
             if (StopTheFirst)
             {
                 if (Result != IntPtr.Zero)
                 {
                     Retorna = new IntPtr[0];
-                    Retorna[0] = (IntPtr)(MappedMemory[i].BaseAddress.ToInt32() + Result.ToInt32());              
-                    return Retorna;
+
+#if WIN64
+                    Retorna[0] = (IntPtr)(MappedMemory[i].BaseAddress + (ulong)Result.ToInt64());        
+#else
+                    Retorna[0] = (IntPtr)(MappedMemory[i].BaseAddress.ToInt64() + Result.ToInt64());
+#endif
+
+                    return;
                 }
 
             }
@@ -213,9 +273,9 @@ public class DotNetScanMemory_SmoLL
                 Retorna[l] = AddressList[l];
             }
             AddressList.Clear();
-            return Retorna;
+            return;
         }
-        return Retorna;
+        return;
     }
 
 
@@ -224,7 +284,7 @@ public class DotNetScanMemory_SmoLL
 
     public bool WriteArray(IntPtr address, string ArrayString)
     {       
-        if (Attacked == (Process)null)
+        if (Attacked == null)
             return false;
         String[] BytesToScan = ArrayString.Split(" "[0]);
         byte[] ArrayWrite = new byte[BytesToScan.Length];
@@ -237,14 +297,14 @@ public class DotNetScanMemory_SmoLL
         }
         // int processHandle = OpenProcess(PROCESS_ALL_ACCESS, false, p.Id);
         return  
-            WriteProcessMemory((int)Attacked.Handle, address.ToInt32(), ArrayWrite, ArrayWrite.Length, 0);
+            WriteProcessMemory(Attacked.Handle, address, ArrayWrite, (uint)ArrayWrite.Length, 0);
        
     }
 
     public Process GetChrome()
     {
        
-        Process Retorna ;
+  
         foreach (Process proc in Process.GetProcessesByName("chrome"))
         {
             try
@@ -262,7 +322,7 @@ public class DotNetScanMemory_SmoLL
 
             }
         }
-        return  (Process)null;
+        return null;
     }
 
 
